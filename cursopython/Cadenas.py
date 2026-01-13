6f22aff27c0671f563bc344af0175faf77f79dc9
Nahuel <agustinnahuelgarcia02@gmail.com>
2026-01-13 17:32:23 -0300

    cadenas y variable

---
+++ cursopython/Cadenas.py
@@ -0,0 +1,12 @@
+# Manejo de Cadenas
+
+mi_cadena = "Nahuel Garcia"
+print(mi_cadena)
+
+# Agregar un salto de linea \n
+mi_cadena = 'Nahuel \nGarcia'
+print(mi_cadena)
+
+#Agregar para agregar un espacio  \t
+mi_cadena = '\tNahuel Garcia'
+print(mi_cadena)
