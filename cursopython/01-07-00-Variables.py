86be0331d308c35bd0606a9fc65397e8ac5ef583
Nahuel <agustinnahuelgarcia02@gmail.com>
2026-01-13 17:22:35 -0300

    manejo variable

---
+++ cursopython/01-07-00-Variables.py
@@ -0,0 +1,13 @@
+# Manejo de Variables
+
+entero = 10  # int - entero
+flotante = 5.7  # float - punto decimal
+cadena = 'Nahuel'  # str (string) - cadena
+
+# Mandar a imprimir (leer el valor de la variable)
+print(entero)
+print(flotante)
+print(cadena)
+
+entero = 15
+print(entero)
