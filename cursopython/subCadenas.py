38eda9ff621e1f1b749aa915589f48b9696dbd98
Nahuel <agustinnahuelgarcia02@gmail.com>
2026-01-13 17:35:15 -0300

    como se maneja las cadenas

---
+++ cursopython/subCadenas.py
@@ -0,0 +1,12 @@
+# Subcadenas en Python
+
+mensaje = 'Hola Mundo'
+#print(mensaje)
+
+# subcadena hola -> cadena[indice_inicio:indice_final + 1]
+subcadena_hola = mensaje[0:4]
+print(subcadena_hola)
+
+subcadena_mundo = mensaje[5:10]
+print(subcadena_mundo)
+
