8fe874a58cb7b12d07c105e2a1f90074aaab8994
Nahuel <agustinnahuelgarcia02@gmail.com>
2026-01-14 14:12:42 -0300

    ciclos

---
+++ cursopython/CicloFor.py
@@ -0,0 +1,8 @@
+print('*** Ciclo for ***')
+cadena = 'Hola Mundo'
+#for letra in cadena:
+    #print(letra, end=' ')
+
+# Imprimir del 1 al 5
+for contador in range(1, 6):
+    print(contador)
---
+++ cursopython/CicloWhile.py
@@ -0,0 +1,10 @@
+print('*** Ciclo while ***')
+# Imprimir del 1 al 5
+contador = 1
+valor_maximo = 5
+while contador <= valor_maximo:
+    print(f'el mumero es: {contador}')
+    contador +=1
+
+
+
