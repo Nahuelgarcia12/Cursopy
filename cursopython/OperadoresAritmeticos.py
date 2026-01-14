70a1647e19f98c3ba48b39e5a26f20c1fe15bade
Nahuel <agustinnahuelgarcia02@gmail.com>
2026-01-14 11:42:33 -0300

    cambio de nombre del archivo + manejo de operadores

---
+++ cursopython/OperadoresAritmeticos.py
@@ -0,0 +1,29 @@
+print('*** Operadores Aritmeticos ***')
+a, b = 10, 5
+print(f'Valor inicial a: {a}')
+print(f'valor inicial b: {b}')
+# Operador de suma +
+print(f'Resultado operador suma a + b es: {a+b}')
+# Operador de resta -
+resultado = a - b
+print(f'Resultado operador resta a - b es: {resultado}')
+# Operador de multiplicacion *
+resultado = a * b
+print(f'Resultado operador multiplicacion a * b es: {resultado} ')
+# Operador de division /
+resultado = a / b
+print(f'Resultado operador division a / b es: {resultado}')
+# Operador de modulo (residuo de la division) %
+resultado = a % b
+print(f'Resultado operador modulo a % b es: {resultado}')
+# Operador exponente (potencia)
+#primera forma
+resultado = 2 ** 3  # base: 2, exponente: 3 = 2*2*2 = 8
+print(f'Resultado operador exponente 2 ** 3 es: {resultado}')
+#segunda forma
+exp=pow(2, 3)
+print(f'Resultado operador exponente 2 elevado 3 es: {exp}')
+
+
+
+
