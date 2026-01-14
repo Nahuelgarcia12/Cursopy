452e6624abd032750e3217626621bab05263b68f
Nahuel <agustinnahuelgarcia02@gmail.com>
2026-01-14 14:01:10 -0300

    operadores y sentencias

---
+++ cursopython/sentencia if.py
@@ -0,0 +1,9 @@
+print('*** Sentencia if elif else')
+dia_con_lluvia = False
+dia_nublado = False
+if dia_con_lluvia:
+    print('Llevar paraguas')
+elif dia_nublado:
+    print('Llevar impermeable')
+else:
+    print('Dejar paraguas e impermeable')
---
+++ cursopython/sentenciaand&or&not.py
@@ -0,0 +1,25 @@
+#ejemplo sentencia and
+edad = 20
+tiene_identificacion = True
+
+if edad >= 18 and tiene_identificacion:
+    print("Puede ingresar")
+else:
+    print("No puede ingresar")
+
+#ejemplo sentencia or
+tiene_entrada = False
+es_invitado = True
+
+if tiene_entrada or es_invitado:
+    print("Puede entrar al evento")
+else:
+    print("No puede entrar al evento")
+ # ejemplo sentencia not
+esta_lloviendo = False
+
+if not esta_lloviendo:
+    print("Puedes salir sin paraguas")
+else:
+    print("Lleva un paraguas")
+
