570777863460a5ad122c6bd6ef5cd2411b87648d
Nahuel <agustinnahuelgarcia02@gmail.com>
2026-03-26 12:02:38 -0300

    agregacion de titulo dinamico

--- cursopython/Zonafit/app.py
+++ cursopython/Zonafit/app.py
@@ -1,13 +1,13 @@
-from  flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
-
+titulo_app = "Zona Fit (GYM)"
  # url : http://localhost:5000/
 @app.route('/')
 @app.route('/index.html')#url:http://localhost:5000/index.html
 def inicio():
   app.logger.debug("Entramos al path de inicio /")
-  return "<p>Hola Mundo</p>"
+  return render_template('index.html', titulo=titulo_app)
 
 if __name__ == '__main__':
     app.run(debug=True)
