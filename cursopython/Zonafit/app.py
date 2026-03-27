a7893cb6cfce402d74b6a9691c74da357a49b6c4
Nahuel <agustinnahuelgarcia02@gmail.com>
2026-03-27 15:13:54 -0300

    creacion de formulario funcional + creacion del metodo guardar

--- cursopython/Zonafit/app.py
+++ cursopython/Zonafit/app.py
@@ -1,17 +1,46 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, redirect, url_for
 import cliente_dao
+from cliente_form import ClienteForm
+from cliente import Cliente
 
 app = Flask(__name__)
 titulo_app = "Zona Fit (GYM)"
- # url : http://localhost:5000/
+app.config['SECRET_KEY'] = 'mundial2026'
+
 @app.route('/')
-@app.route('/index.html')#url:http://localhost:5000/index.html
+@app.route('/index.html')
 def inicio():
-  app.logger.debug("Entramos al path de inicio /")
-  #recuperamos datos de la bd
-  cliente_db=cliente_dao.ClienteDAO.seleccionar()
-  return render_template('index.html', titulo=titulo_app,clientes=cliente_db)
+    app.logger.debug("Entramos al path de inicio /")
+    clientes_db = cliente_dao.ClienteDAO.seleccionar()
+    cliente = Cliente()
+    cliente_forma = ClienteForm(obj=cliente)
+    return render_template('index.html', titulo=titulo_app, clientes=clientes_db, forma=cliente_forma)
+
+@app.route('/guardar', methods=['POST'])
+def guardar():
+    cliente = Cliente()
+    cliente_forma = ClienteForm(obj=cliente)
+    if cliente_forma.validate_on_submit():
+        #llenamos el objeto cliente con los valores del form
+        cliente_forma.populate_obj(cliente)
+        #guardo al nuevo cliente en bd
+        cliente_dao.ClienteDAO.insertar(cliente)
+    return redirect(url_for('inicio'))
+
+@app.route('/editar/<int:id>')
+def editar(id):
+    cliente =cliente_dao.ClienteDAO.seleccionar_por_id(id)
+    cliente_forma = ClienteForm(obj=cliente)
+    clientes_db = cliente_dao.ClienteDAO.seleccionar()
+    return render_template('index.html',titulo=titulo_app,
+                           clientes=clientes_db, forma=cliente_forma)
+
+
+@app.route('/limpiar')
+def limpiar():
+    return redirect(url_for('inicio'))
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ cursopython/Zonafit/cliente_form.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField ,SubmitField
+from wtforms.validators import DataRequired
+
+
+class ClienteForm(FlaskForm):
+    nombre = StringField('Nombre',validators=[DataRequired()])
+    apellido = StringField('Apellido',validators=[DataRequired()])
+    membresia = IntegerField('Membresia',validators=[DataRequired()])
+    guardar = SubmitField('Guardar')
